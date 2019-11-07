25be60768afbaa0cda58734ad37c615fa1e792e3
himanshu12245 <himanshu12245@gmail.com>
2019-11-07 11:11:31 +0530

    Add files via upload

---
+++ D.C
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+printf("\tth																																																																																																	is is after a tab\vthis is after vertical tab\nthis is after a new line\r this is after a carriage return\b");
+getch();
+}
